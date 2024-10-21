a0771f8233a390414686b907d42a43b30818e055
kamruzzaman1234 <dmkkaarr.labib980@gmail.com>
2024-10-21 23:45:34 +0600

    code add

--- Provider/AuthProvider.jsx
+++ Provider/AuthProvider.jsx
@@ -1,6 +1,8 @@
 import { createContext, useEffect, useState } from "react";
 import auth from "../Firebase/Firebase.config";
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, 
+signInWithEmailAndPassword, signOut } from "firebase/auth";
+import axios from "axios"
 
 export const AuthContext = createContext();
 
@@ -25,9 +27,29 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email
+            const logInEmailUser = {email: userEmail}
             setUser(currentUser);
             setLoading(false);
+            if(currentUser){
+                
+                
+                axios.post('https://real-state-project-server.onrender.com/jwt', logInEmailUser, {withCredential: true})
+                .then(res=> {
+                    console.log(res.data)
+                })
+                
+                
+            }else{
+                axios.post('https://real-state-project-server.onrender.com/logout',
+             logInEmailUser, {withCredentials: true})
+             .then(res=>{
+                console.log(res.data)
+             })
+            }
+           
         });
+
 
         return () => unsubscribe();  // Corrected the cleanup function
     }, []);
